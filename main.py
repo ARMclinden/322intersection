1979d3af48e1490f02c9aaecca083aa548cf5774
Andrew McLinden <amclinden@mtech.edu>
2026-03-02 12:48:58 -0700

    added check_intersect
    does not work yet, need update to details

--- main.py
+++ main.py
@@ -27,3 +27,34 @@ def find_orientation(x1 : tuple, x2 : tuple, x3: tuple):
         return 1
     else:
         return 2
+    
+def check_intersect(l1, l2):
+    if l1 == () or l2 == ():
+        return -1
+    
+    orient1 = find_orientation((l1[0], l1[1]), (l1[2], l1[3]), (l2[0], l2[1]))
+    orient2 = find_orientation((l1[0], l1[1]), (l1[2], l1[3]), (l2[2], l2[3]))
+    orient3 = find_orientation((l2[0], l2[1]), (l2[2], l2[3]), (l1[0], l1[1]))
+    orient4 = find_orientation((l2[0], l2[1]), (l2[2], l2[3]), (l1[2], l1[3])) 
+
+    intersect = 0
+    
+    if orient1 != orient2:
+        if orient3 != orient4:
+            intersect = 1
+        else:
+            intersect = 0
+    else: 
+        intersect = 0
+    
+    if orient1 == 0 and orient2 == 0 and orient3 == 0 and orient4 == 0:
+        if check_intersect((l1[0], 0, l1[2], 0), (l2[0], 0, l2[2], 0)) == 1 and check_intersect((0, l1[1], 0, l1[3]), (0, l2[1], 0, l2[3])) == 1:
+            if intersect == 1:
+                intersect = 0
+            else:
+                intersect = 1
+
+    return intersect
+
+if __name__ == "__main__":
+    check_intersect((0,0,1,1), (1,1,2,2))
