98935e80ddd114e32c2a5e7fd2ead9a2c4a8fdf8
Andrew McLinden <amclinden@mtech.edu>
2026-03-04 12:45:11 -0700

    completed tests

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
-from SegmentIntersection import find_orientation
+from SegmentIntersection import find_orientation, on_segment, do_intersect
 
-class TestOrientation(unittest.TestCase):
+class TestIntersect(unittest.TestCase):
     def test_clockwise(self):
         self.assertEqual(find_orientation((0,0), (1,1), (2, 1)), 1)
 
@@ -25,6 +25,30 @@ class TestOrientation(unittest.TestCase):
     
     def test_missing_input3(self):
         self.assertEqual(find_orientation((0, 0), (1, 1), ()), -1)
+    
+    def test_on_segment_true(self):
+        self.assertEqual(on_segment((0,0), (1,1), (2,2)), True)
+
+    def test_on_segment_false(self):
+        self.assertEqual(on_segment((0,0), (2,2), (1,1)), False)
+
+    def test_on_segment_fail(self):
+        self.assertEqual(on_segment((), (1,1), (2,2)), -1)
+    
+    def test_intersect(self):
+        self.assertEqual(do_intersect(((1, 1), (4, 4)), ((2, 4), (4,2))), True)
+
+    def test_intersect_wrong(self):
+        self.assertEqual(do_intersect(((1, 1), (4, 4)), ((1, -1), (2,-4))), False)
+
+    def test_intersect_collinear(self):
+        self.assertEqual(do_intersect(((0, 0), (1, 1)), ((1, 1), (2, 2))), True)
+    
+    def test_intersect_collinear_wrong(self):
+        self.assertEqual(do_intersect(((0, 0), (1, 1)), ((2, 2), (3, 3))), False)
+    
+    def test_intersect_bad_input(self):
+        self.assertEqual(do_intersect((),()), -1)
 
 if __name__ == "__main__":
     unittest.main()
