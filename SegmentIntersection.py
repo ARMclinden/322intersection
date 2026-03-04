07de8c4746e67b00462446d0ab3fe4e858038eb0
Andrew McLinden <amclinden@mtech.edu>
2026-03-04 12:28:09 -0700

    Completed assignment requirements

--- SegmentIntersection.py
+++ SegmentIntersection.py
@@ -14,7 +14,7 @@
 # university's academic integrity policy. I understand the importance the consequences of plagiarism.
 
 
-def find_orientation(p : tuple, q : tuple, r: tuple):
+def find_orientation(p, q, r):
 
     if p == () or q == () or r == ():
         return -1
@@ -29,27 +29,39 @@ def find_orientation(p : tuple, q : tuple, r: tuple):
         return 2
     
 def on_segment(p, q, r):
-    1;
+
+    if p == () or q == () or r == ():
+        return -1
+    
+    n = find_orientation(p, q, r)
+    
+    if n == 0 and q[0] >= p[0] and q[0] <= r[0] and q[1] >= p[1] and q[1] <= r[1]:
+        return True
+    else:
+        return False
 
 def do_intersect(l1, l2):
     if l1 == () or l2 == ():
         return -1
     
-    orient1 = find_orientation((l1[0], l1[1]), (l1[2], l1[3]), (l2[0], l2[1]))
-    orient2 = find_orientation((l1[0], l1[1]), (l1[2], l1[3]), (l2[2], l2[3]))
-    orient3 = find_orientation((l2[0], l2[1]), (l2[2], l2[3]), (l1[0], l1[1]))
-    orient4 = find_orientation((l2[0], l2[1]), (l2[2], l2[3]), (l1[2], l1[3])) 
+    orient1 = find_orientation(l1[0], l1[1], l2[0])
+    orient2 = find_orientation(l1[0], l1[1], l2[1])
+    orient3 = find_orientation(l2[0], l2[1], l1[0])
+    orient4 = find_orientation(l2[0], l2[1], l1[1])
 
-    intersect = 0
+    intersect = False
+    intersect2 = False
     
-    if orient1 != orient2:
-        if orient3 != orient4:
-            intersect = 1
-        else:
-            intersect = 0
-    else: 
-        intersect = 0
+    if orient1 != orient2 and orient3 != orient4:
+        intersect = True
     
-    if orient1 == 0 and orient2 == 0 and orient3 == 0 and orient4 == 0:
-        1;
-    return intersect
+    if orient1 == 0 and on_segment(l1[0], l2[0], l1[1]):
+        intersect2 = True
+    elif orient2 == 0 and on_segment(l1[0], l2[1], l1[1]):
+        intersect2 = True
+    elif orient3 == 0 and on_segment(l2[0], l1[0], l2[1]):
+        intersect2 = True
+    elif orient4 == 0 and on_segment(l2[0], l1[1], l2[1]):
+        intersect2 = True
+
+    return intersect ^ intersect2
